8de5cf695c680370cd33bbbc780a5d67398090ab
Pierre-Marie Dartus <p.dartus@salesforce.com>
2019-02-17 09:51:37 +0100

    add expected name

--- .github/main.workflow
+++ .github/main.workflow
@@ -1,19 +1,9 @@
 workflow "My Action" {
   on = "push"
-  resolves = ["Hello world", "Test checks"]
+  resolves = ["Performance benchmark"]
 }
 
-action "Hello world" {
-  uses = "./actions/hello-world"
-
-  env = {
-      MY_NAME = "Foo"
-  }
-
-  args = "\"Hello world, I'm $MY_NAME!\""
-}
-
-action "Test checks" {
+action "Performance benchmark" {
     uses = "./actions/test-checks"
     secrets = ["GITHUB_TOKEN"]
 }
--- actions/test-checks/run.js
+++ actions/test-checks/run.js
@@ -9,7 +9,7 @@ const {
 } = repository;
 const { name: repo } = repository;
 
-const checkName = 'My check';
+const checkName = 'Performance checks';
 
 const headers = {
     'Content-Type': 'application/json',
@@ -18,6 +18,52 @@ const headers = {
     'User-Agent': 'eslint-action',
 };
 
+const text = `
+## Summary results
+
+### :white_check_mark: Improvements
+
+benchmark | base(\`808471b\`) | target(\`3440432\`) | trend
+--- | --- | --- | ---
+benchmark-table-wc/append/1k | 273.95 (±26.20 ms) | 255.65 (±6.60 ms) | -18.3ms (6.7%)
+benchmark-table-wc/clear/1k | 38.70 (±4.55 ms) | 22.55 (±2.60 ms) | -16.2ms (41.7%)
+
+### :x: Regressions
+
+benchmark | base(\`808471b\`) | target(\`3440432\`) | trend
+--- | --- | --- | ---
+benchmark-table/create/10k | 883.35 (±5.50 ms) | 889.55 (±7.75 ms) | +6.2ms (0.7%)
+benchmark-table-component/append/1k | 247.80 (±14.10 ms) | 253.70 (±5.35 ms) | +5.9ms (2.4%)
+benchmark-table-component/create/10k | 1788.25 (±11.30 ms) | 1806.85 (±10.75 ms) | +18.6ms (1.0%)
+benchmark-table-component/create/1k | 214.90 (±5.75 ms) | 220.50 (±6.10 ms) | +5.6ms (2.6%)
+
+
+## Full results
+
+<details>
+ <summary>Full result</summary>
+ 
+benchmark | base(\`808471b\`) | target(\`3440432\`) | trend
+--- | --- | --- | ---
+benchmark-table/append/1k | 151.55(±3.80 ms) | 150.05 (±4.05 ms) | -1.5ms (1.0%) 👌
+benchmark-table/clear/1k | 6.20 (±0.40 ms) | 5.95 (±0.25 ms) | -0.3ms (4.0%) 👌
+benchmark-table/create/10k | 883.35 (±5.50 ms) | 889.55 (±7.75 ms) | +6.2ms (0.7%) 👎
+benchmark-table/create/1k | 117.75 (±2.55 ms) | 116.60 (±3.30 ms) | -1.2ms (1.0%) 👌
+benchmark-table/update-10th/1k | 85.60 (±1.35 ms) | 83.95 (±3.50 ms) | -1.6ms (1.9%) 👍
+benchmark-table-component/append/1k | 247.80 (±14.10 ms) | 253.70 (±5.35 ms) | +5.9ms (2.4%) 👎
+benchmark-table-component/clear/1k | 11.45 (±1.30 ms) | 12.10 (±2.10 ms) | +0.7ms (5.7%) 👌
+benchmark-table-component/create/10k | 1788.25 (±11.30 ms) | 1806.85 (±10.75 ms) | +18.6ms (1.0%) 👎
+benchmark-table-component/create/1k | 214.90 (±5.75 ms) | 220.50 (±6.10 ms) | +5.6ms (2.6%) 👎
+benchmark-table-component/update-10th/1k | 70.55 (±5.50 ms) | 70.35 (±5.25 ms) | -0.2ms (0.3%) 👌
+benchmark-table-wc/append/1k | 273.95 (±26.20 ms) | 255.65 (±6.60 ms) | -18.3ms (6.7%) 👍
+benchmark-table-wc/clear/1k | 38.70 (±4.55 ms) | 22.55 (±2.60 ms) | -16.2ms (41.7%) 👍
+benchmark-table-wc/create/10k | 2017.10 (±18.45 ms) | 2013.70 (±22.25 ms) | -3.4ms (0.2%) 👌
+benchmark-table-wc/create/1k | 228.75 (±5.10 ms) | 229.15 (±4.80 ms) | +0.4ms (0.2%) 👌
+benchmark-table-wc/update-10th/1k | 67.45 (±5.25 ms) | 70.20 (±4.40 ms) | +2.7ms (4.1%) 👌
+
+</details>
+`;
+
 async function createCheck() {
     const body = {
         name: checkName,
@@ -25,12 +71,11 @@ async function createCheck() {
         status: 'completed',
         started_at: new Date(),
         completed_at: new Date(),
-        conclusion: 'success',
+        conclusion: 'failure',
         output: {
-            title: 'Test checks',
-            summary: 'There are 0 failures, 2 warnings, and 1 notices.',
-            text:
-                'You may have some misspelled words on lines 2 and 4. You also may want to add a section in your README about how to install your app.',
+            title: 'Performance benchmark',
+            summary: 'There are 4 benchmarks that regressed and 2 benchmarks improved.',
+            text,
         },
     };
 
